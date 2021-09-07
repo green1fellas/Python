03f559389e988873538877c10e6e2babfc206f61
Scott Obernier <scottobernier@gmail.com>
2021-09-06 22:57:22 -0400

    eurler 15

--- ProjectEuler/15.py
+++ ProjectEuler/15.py
@@ -1,21 +1,23 @@
-import winsound
-
 size = 20
-routes = 0
+memorization = []
+for i in range(0, size+1):
+    memorization.append([])
+    for j in range(0, size+1):
+        memorization[i].append(-1)
 
 def move(R, D, Rm, Dm):
-    global routes
-
-    if(R < Rm):
-        move(R+1, D, Rm, Dm)
-    if(D < Dm):
-        move(R, D+1, Rm, Dm)
-
-    if(R == Rm and D == Dm):
-        routes = routes + 1
+    routes = 0
+    if(memorization[D][R] != -1):
+        return memorization[D][R]
+    else:
+        if(R < Rm):
+            routes = routes + move(R+1, D, Rm, Dm)
+        if(D < Dm):
+            routes = routes + move(R, D+1, Rm, Dm)
+        if(R == Rm and D == Dm):
+            routes = routes + 1
+        memorization[D][R] = routes
+        return routes
 
 print("Working...")
-move(0, 0, size, size)
-print(routes)
-winsound.Beep(1000, 500)
-    
+print(move(0, 0, size, size))
